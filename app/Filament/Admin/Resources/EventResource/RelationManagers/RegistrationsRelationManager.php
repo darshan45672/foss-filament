f491cfd68c0abf81a73be1143848d9f958bd77be
darshan45672 <drshnbhandary@gmail.com>
2025-08-31 08:31:05 +0530

    done relationship manager

---
+++ app/Filament/Admin/Resources/EventResource/RelationManagers/RegistrationsRelationManager.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Filament\Admin\Resources\EventResource\RelationManagers;
+
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Tables;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\ToggleButtons;
+use Filament\Forms\Components\Section;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Columns\BadgeColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class RegistrationsRelationManager extends RelationManager
+{
+    protected static string $relationship = 'registrations';
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Section::make('Event Registeration Details')
+                    ->collapsible()
+                    ->schema([
+                        Select::make('event_id')->required()->relationship('event', 'name')->preload()->searchable(),
+                        Select::make('user_id')->required()->relationship('user', 'name')->preload()->searchable(),
+                        ToggleButtons::make('status')->options([
+                            'pending' => 'Pending',
+                            'approved' => 'Approved',
+                            'rejected' => 'Rejected'
+                        ])->required()->default('pending')->inline()
+                        ->colors([
+                            'pending' => 'warning',
+                            'approved' => 'success',
+                            'rejected' => 'danger',
+                        ])->icons([
+                            'pending' => 'heroicon-o-clock',
+                            'approved' => 'heroicon-o-check-circle',
+                            'rejected' => 'heroicon-o-x-circle'
+                        ]),
+                    ])->columns(2),
+            ]);
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->recordTitleAttribute('id')
+            ->columns([
+               TextColumn::make('event.name')
+                    ->sortable(),
+                TextColumn::make('user.name')
+                    ->sortable(),
+                BadgeColumn::make('status')
+                    ->colors([
+                        'warning' => 'pending',
+                        'success' => 'approved',
+                        'danger' => 'rejected',
+                    ])
+                    ->sortable()
+                    ->searchable(),
+
+                TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->headerActions([
+                Tables\Actions\CreateAction::make(),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
